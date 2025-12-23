7e3192c00df44d3418d340cd454452d252733337
gil-son <gilszon@gmail.com>
2025-12-22 21:45:57 -0300

    feat: organized projects

--- projects/11-langchain/basic-openai-connection-with-langserve-and-client/client.py
+++
@@ -1,7 +0,0 @@
-from langserve import RemoteRunnable
-
-remote_chain = RemoteRunnable("http://localhost:8000/translator")
-
-text = remote_chain.invoke({"idiom": "Franch", "text": "I love pizza!"})
-
-print(text)
