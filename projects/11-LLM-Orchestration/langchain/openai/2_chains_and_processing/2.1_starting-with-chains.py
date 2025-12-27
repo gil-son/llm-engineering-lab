1eb57cf13744753b619a0080c478a470c8cb47dc
gil-son <gilszon@gmail.com>
2025-12-26 21:36:30 -0300

    feat: langchain - chains and processing with openai model

---
+++ projects/11-LLM-Orchestration/langchain/openai/2_chains_and_processing/2.1_starting-with-chains.py
@@ -0,0 +1,20 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+prompt = ChatPromptTemplate.from_template(
+    "Hi, I'm {name}! Tell me a joke with my name!"
+)
+
+# 2. Initialize the model
+model = ChatOpenAI(model="gpt-4.1-nano", temperature=0.5)
+
+# 3. Create the chain
+chain = prompt | model
+
+# 4. Invoke the chain with variables
+result = chain.invoke({"name": "Goku"})
+
+# 5. Output
+print(result.content)
